734b8dd288387b65336e9d0ec5ce6acefcf24216
Chris Nunes <chrisnunes57@gmail.com>
2018-09-29 16:21:10 -0500

    final version?

--- background.js
+++ background.js
@@ -5,21 +5,31 @@ window.onload = function(){
     span.id="chat-apa-hack-toggle";
     span.innerHTML = "+";
     container.className = "chat-apa-hack-hidden";
+    container.id = "chat-apa-hack-container";
+    chatWindow.src = 'https://webchat.botframework.com/embed/Parvo-Bot-2?s=3DjFldoc7pE.cwA.CLA.m1R0F63gvN2KR0EXoWtBih_Mat1P2d0n3MEuXmICBfY';
+    chatWindow.id = "chat-apa-hack";
+    let ringContainer = document.createElement("div");
+    ringContainer.className = "ring-container";
+    let ringring = document.createElement("div");
+    ringring.className = "ringring";
+    let circle = document.createElement("div");
+    circle.className = "circle";
     span.onclick = function(){
         if(container.classList.contains("chat-apa-hack-hidden")){
             container.classList.remove("chat-apa-hack-hidden");
             span.classList.remove("chat-apa-hack-hidden");
             span.innerHTML = "-";
+            ringContainer.classList.add("chat-apa-hack-hidden");
         }else{
             span.classList.add("chat-apa-hack-hidden");
             container.classList.add("chat-apa-hack-hidden");
             span.innerHTML = "+";
         }
     }
-    container.id = "chat-apa-hack-container";
-    chatWindow.src = 'https://webchat.botframework.com/embed/Parvo-Bot-2?s=3DjFldoc7pE.cwA.CLA.m1R0F63gvN2KR0EXoWtBih_Mat1P2d0n3MEuXmICBfY';
-    chatWindow.id = "chat-apa-hack";
+    ringContainer.appendChild(ringring);
+    ringContainer.appendChild(circle);
     container.appendChild(chatWindow);
     container.appendChild(span);
+    container.appendChild(ringContainer);
     document.body.appendChild(container);
 }

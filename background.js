bbaaaa4ba62fba011f7bd983ffd9cd2b40f5b253
Chris Nunes <chrisnunes57@gmail.com>
2018-09-29 12:46:54 -0500

    barebones extension

---
+++ background.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    let chatWindow = document.createElement("iframe");
+    chatWindow.src = 'https://webchat.botframework.com/embed/Parvo-Bot-2?s=3DjFldoc7pE.cwA.CLA.m1R0F63gvN2KR0EXoWtBih_Mat1P2d0n3MEuXmICBfY';
+    document.body.appendChild(chatWindow);
+}
